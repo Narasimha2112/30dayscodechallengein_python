537a5cb6f2c294eaa4e351f1e511e1b147e471dd
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-10 20:29:32 +0530

    Reverse a string

---
+++ Day19.py
@@ -0,0 +1,18 @@
+#Day 19
+
+"""
+Question:
+Write a program that reverses a given string.
+
+Example:
+Input: ""hello""
+Output: ""olleh""
+
+Your Task:
+Develop a program to reverse a given string.
+"""
+
+
+str1 = input("Enter the string: ")[::-1]
+
+print("Reversed string:",str1)
