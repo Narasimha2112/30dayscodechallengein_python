3ba024c3c7118463beef50b1e9f1c49be7c8be20
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-22 00:06:25 +0530

    Create Day25.py

---
+++ Day25.py
@@ -0,0 +1,22 @@
+#Day 25
+
+"""
+Question:
+Write a program that prints the index of a given character in a given string.
+
+Example:
+String: "hello"
+Character: 'o'
+Index: 4
+
+Your Task:
+Develop a program in your preferred programming language to print the index of a given character in a given string.
+"""
+
+str1 = input()
+
+character = input()
+
+print("String:",str1)
+print("Character:",character)
+print("Index:",str1.find(character))
