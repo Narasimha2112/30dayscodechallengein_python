78d90c36336b40c5b07c29ba60af1a6f448d53a5
Narasimha2112 <141565501+Narasimha2112@users.noreply.github.com>
2024-03-21 11:48:51 +0530

    Create Day23.py

---
+++ Day23.py
@@ -0,0 +1,16 @@
+#Day 23
+
+"""
+Question:
+Write a program that swaps the cases of characters in a given string.
+
+Example:
+String: "Hello World"
+Output: "hELLO wORLD"
+
+Your Task:
+Develop a program to swap the cases of characters in a given string.
+"""
+
+str1 = input()
+print(str1.swapcase())
